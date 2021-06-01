ecae9023f0ed0cba39e9d3717a3c185add34cccf
pyrus-vagus <grushadaria@gmail.com>
2021-06-01 20:42:42 +0300

    implement functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnsOpenModal.forEach((i) => i.addEventListener("click", openModal));
+btnCloseModal.addEventListener("click", closeModal);
